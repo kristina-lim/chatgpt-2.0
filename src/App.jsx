f693db21439d7b457bcbd43067050fe919d43328
kristina-lim <kristinahlim@gmail.com>
2023-02-21 13:22:10 -0800

    Have AI photo render on page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Configuration, OpenAIApi }  from 'openai';
 import './App.css'
 
 export default function App() {
-  const [prompt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
   });
@@ -12,20 +13,24 @@ export default function App() {
 
   const generateImage = async () => {
     const res = await openai.createImage({
-      prompt: "Say this is a test",
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
 
     });
-
-    console.log(res.data.data[0].url);
+    setResult(res.data.data[0].url);
   }
 
   return (
     <div className="app-main">
       <h3>Generate an Image using OpenAI API</h3>
-      <input className="app-input" />
+      <input 
+        className="app-input"
+        placeholder="Type something to generate an image"
+        onChange={(evt) => setPrompt(evt.target.value)} />
       <button onClick={generateImage}>Generate an Image</button>
+
+      {result.length > 0 ? <img className="result-img" src={result} alt="result" /> : <></>}
     </div>
   )
 }
